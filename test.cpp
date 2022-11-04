a60412eebb5afaf7f1bea3df9a7a09d0cd241690
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-04 23:22:53 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,102 +6,100 @@
 #include "stl_additions.h"
 using namespace std;
 int main() {
-    int *a1 = new int[5];
-    for (int i = 0; i < 5; i++) a1[i] = i;
-    cout<<"1D Dynamic Array: ";
-    astl::show(a1, 5, true);
-    
-    int **a2 = new int*[3];
-    for (int i = 0; i < 3; i++) {
-        a2[i] = new int[3];
-        for (int j = 0; j < 3; j++) a2[i][j] = i + j;
-    }
-    cout<<"2D Dynamic Array: ";
-    astl::show(a2, 3, 3, true);
+    // Example data
+    int *array_1D = new int[5];                         // array_1D = [0, 1, 2, 3, 4]
+    for (int i = 0; i < 5; i++) array_1D[i] = i;
+    int *array_1D_2 = new int[8];                       // array_1D_2 = [0, 2, 4, 6, 8, 10, 12, 14]
+    for (int i = 0; i < 8; i++) array_1D_2[i] = i * 2;
+    int **array_2D = new int*[3];                       // array_2D = [[0, 1], [1, 2], [2, 3]]
+    for (int i = 0; i < 3; i++) array_2D[i] = new int[2];
+    for (int i = 0; i < 3; i++) for (int j = 0; j < 2; j++) array_2D[i][j] = i + j;
+    int **array_2D_2 = new int*[4];                     // array_2D_2 = [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6], [0, 3, 6, 9]]
+    for (int i = 0; i < 4; i++) array_2D_2[i] = new int[4];
+    for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) array_2D_2[i][j] = i * j;
+    vector < int > vec_1 = {1, 3, 5, 7, 9, 11};
+    vector < int > vec_2 = {0, 2, 4, 6, 8};
+    list < int > list_1;                                // list_1 = [0, 3, 6, 9, 12]
+    for (int i = 0; i < 5; i++) list_1.push_back(i * 3);
+    list < int > list_2;                                // list_2 = [3, 4, 5, 6]
+    for (int i = 0; i < 4; i++) list_2.push_back(i + 3);
+    set < int > set_1;                                  // set_1 = {1, 2, 3, 4}
+    for (int i = 0; i < 4; i++) set_1.insert(i + 1);
+    set < int > set_2;                                  // set_2 = {3, 4, 5, 6, 7, 8, 9}
+    for (int i = 0; i < 7; i++) set_2.insert(i + 3);
+    map <char, int> map_1;                              // map_1 = {A: 0, B: 1, C: 2}
+    for (int i = 0; i < 3; i++) map_1.insert(make_pair((char) (i + 65), i));
+    map <char, int> map_2;                              // map_2 = {C: 1, D: 2, E: 3, F: 4}
+    for (int i = 0; i < 4; i++) map_2.insert(make_pair((char) (i + 67), i + 1));
 
-    vector < int > v1 = {1, 2, 3, 4};
-    cout<<"1D Vector: ";
-    astl::show(v1, true);
+    // Show functions
+    /*
+    astl::show(array_1D, 5, true);
+    astl::show(array_2D, 3, 2, true);
+    astl::show(vec_1, true);
+    astl::show(list_1, true);
+    astl::show(set_1, true);
+    astl::show(map_1, true);*/
 
-    vector < vector < double > > v2 = {{1.5, 2.07}, {3.058, 4.7}};
-    cout<<"2D Vector: ";
-    astl::show(v2, true);
+    // Test adding two containers together
+    /*
+    int aa_size;
+    int *array_added = astl::add(array_1D, 5, array_1D_2, 8, aa_size);
+    astl::show(array_added, aa_size, true);
+    int aa_size1, aa_size2;
+    int **array2D_added = astl::add(array_2D, 3, 2, array_2D_2, 4, 4, aa_size1, aa_size2);
+    astl::show(array2D_added, aa_size1, aa_size2, true);
+    astl::show(astl::add(vec_1, vec_2), true);
+    astl::show(astl::add(list_1, list_2), true);
+    astl::show(astl::add(set_1, set_2), true);
+    astl::show(astl::add(map_1, map_2), true);*/
 
-    vector < int > *dv = new vector < int >[3];
-    for (int i = 0; i < 3; i++) {
-        dv[i] = {i, i+1, i+2};
-    }
-    cout<<"1D Dynamic Array of Vectors: ";
-    astl::show(dv, 3, true);
+    // Test operation with constants on containers
+    /*
+    astl::show(astl::opr(array_1D, 5, op::add, 2), 5, true);
+    astl::show(astl::opr(array_1D, 5, op::subtract, 2), 5, true);
+    astl::show(astl::opr(array_1D, 5, op::multiply, 2), 5, true);
+    astl::show(astl::opr(array_1D, 5, op::divide, 2), 5, true);
+    astl::show(astl::opr(array_1D, 5, op::power, 2), 5, true);
+    astl::show(astl::opr(array_1D, 5, op::mod, 2), 5, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::add, 2), 3, 2, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::subtract, 2), 3, 2, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::multiply, 2), 3, 2, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::divide, 2), 3, 2, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::power, 2), 3, 2, true);
+    astl::show(astl::opr(array_2D, 3, 2, op::mod, 2), 3, 2, true);
+    astl::show(astl::opr(vec_1, op::add, 2), true);
+    astl::show(astl::opr(vec_1, op::subtract, 2), true);
+    astl::show(astl::opr(vec_1, op::multiply, 2), true);
+    astl::show(astl::opr(vec_1, op::divide, 2), true);
+    astl::show(astl::opr(vec_1, op::power, 2), true);
+    astl::show(astl::opr(vec_1, op::mod, 2), true);
+    astl::show(astl::opr(list_1, op::add, 2), true);
+    astl::show(astl::opr(list_1, op::subtract, 2), true);
+    astl::show(astl::opr(list_1, op::multiply, 2), true);
+    astl::show(astl::opr(list_1, op::divide, 2), true);
+    astl::show(astl::opr(list_1, op::power, 2), true);
+    astl::show(astl::opr(list_1, op::mod, 2), true);
+    astl::show(astl::opr(set_1, op::add, 2), true);
+    astl::show(astl::opr(set_1, op::subtract, 2), true);
+    astl::show(astl::opr(set_1, op::multiply, 2), true);
+    astl::show(astl::opr(set_1, op::divide, 2), true);
+    astl::show(astl::opr(set_1, op::power, 2), true);
+    astl::show(astl::opr(set_1, op::mod, 2), true);
+    astl::show(astl::opr(map_1, op::add, 2), true);
+    astl::show(astl::opr(map_1, op::subtract, 2), true);
+    astl::show(astl::opr(map_1, op::multiply, 2), true);
+    astl::show(astl::opr(map_1, op::divide, 2), true);
+    astl::show(astl::opr(map_1, op::power, 2), true);
+    astl::show(astl::opr(map_1, op::mod, 2), true);*/
 
-    list<string> l;
-    l.push_back("Red");
-    l.push_back("Green");
-    l.push_back("Blue");
-    cout<<"STL List: ";
-    astl::show(l, true);
-
-    set <char> s;
-    s.insert('C');
-    s.insert('B');
-    s.insert('A');
-    cout<<"STL Set: ";
-    astl::show(s, true);
-
-    map <char, int> m;
-    m.insert(make_pair('B', 1));
-    m.insert(make_pair('C', 2));
-    m.insert(make_pair('A', 0));
-    cout<<"STL Map: ";
-    astl::show(m, true);
-
-    int *a3 = new int[8];
-    for (int i = 0; i < 8; i++) a3[i] = i * 2;
-    int as_size;
-    int *as = astl::add(a1, 5, a3, 8, as_size);
-    cout<<"Adding arrays: ";
-    astl::show(as, as_size);
-
-    int **a4 = new int*[5];
-    for (int i = 0; i < 5; i++) {
-        a4[i] = new int[4];
-        for (int j = 0; j < 4; j++) a4[i][j] = i * j;
-    }
-    int as_size1, as_size2;
-    int **as2 = astl::add(a2, 3, 3, a4, 5, 4, as_size1, as_size2);
-    cout<<"Adding 2D arrays: ";
-    astl::show(as2, as_size1, as_size2);
-
-    vector < double > v3 = {4, 3, 2, 1, 5};
-    cout<<"Adding vectors: ";
-    astl::show(astl::add(v1, v3));
-
-    list <int> l1;
-    for (int i = 0; i < 4; i++) l1.push_back(i);
-    list <int> l2;
-    for (int i = 0; i < 3; i++) l2.push_back((i+1) * 10);
-    cout<<"Adding lists: ";
-    astl::show(astl::add(l1, l2));
-
-    set <int> s1;
-    for (int i = 0; i < 5; i++) s1.insert(i);
-    set <int> s2;
-    for (int i = 2; i < 10; i++) s2.insert(i);
-    cout<<"Adding sets: ";
-    astl::show(astl::add(s1, s2));
-
-    map <char, int> m2;
-    m2.insert(make_pair('C', 3));
-    m2.insert(make_pair('D', 4));
-    m2.insert(make_pair('E', 5));
-    cout<<"Adding maps: ";
-    astl::show(astl::add(m, m2));
-
-    delete[] a1;
-    for (int i = 0; i < 3; i++) delete a2[i];
-    delete[] a2;
-    delete[] a3;
-    delete[] as;
+    // Free memory to prevent leaks
+    delete[] array_1D;
+    delete[] array_1D_2;
+    for (int i = 0; i < 3; i++) delete[] array_2D[i];
+    delete[] array_2D;
+    for (int i = 0; i < 4; i++) delete[] array_2D_2[i];
+    delete[] array_2D_2;
     cin.ignore();
     cin.get();
     return 0;
