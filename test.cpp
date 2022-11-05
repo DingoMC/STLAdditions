1786aa1c5df8383a47b9ea70760af17f995f59b7
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-05 12:36:35 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,18 +9,18 @@ int main() {
     // Example data
     int *array_1D = new int[5];                         // array_1D = [0, 1, 2, 3, 4]
     for (int i = 0; i < 5; i++) array_1D[i] = i;
-    int *array_1D_2 = new int[8];                       // array_1D_2 = [0, 2, 4, 6, 8, 10, 12, 14]
-    for (int i = 0; i < 8; i++) array_1D_2[i] = i * 2;
+    int *array_1D_2 = new int[8];                       // array_1D_2 = [2, 4, 6, 8, 10, 12, 14, 16]
+    for (int i = 0; i < 8; i++) array_1D_2[i] = (i + 1) * 2;
     int **array_2D = new int*[3];                       // array_2D = [[0, 1], [1, 2], [2, 3]]
     for (int i = 0; i < 3; i++) array_2D[i] = new int[2];
     for (int i = 0; i < 3; i++) for (int j = 0; j < 2; j++) array_2D[i][j] = i + j;
-    int **array_2D_2 = new int*[4];                     // array_2D_2 = [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6], [0, 3, 6, 9]]
+    int **array_2D_2 = new int*[4];                     // array_2D_2 = [[1, 1, 1, 1], [2, 4, 6, 8], [3, 6, 9, 12], [4, 8, 12, 16]]
     for (int i = 0; i < 4; i++) array_2D_2[i] = new int[4];
-    for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) array_2D_2[i][j] = i * j;
+    for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) array_2D_2[i][j] = (i + 1) * (j + 1);
     vector < int > vec_1 = {1, 3, 5, 7, 9, 11};
     vector < int > vec_2 = {0, 2, 4, 6, 8};
-    list < int > list_1;                                // list_1 = [0, 3, 6, 9, 12]
-    for (int i = 0; i < 5; i++) list_1.push_back(i * 3);
+    list < int > list_1;                                // list_1 = [3, 6, 9, 12, 15]
+    for (int i = 0; i < 5; i++) list_1.push_back((i + 1) * 3);
     list < int > list_2;                                // list_2 = [3, 4, 5, 6]
     for (int i = 0; i < 4; i++) list_2.push_back(i + 3);
     set < int > set_1;                                  // set_1 = {1, 2, 3, 4}
@@ -41,57 +41,25 @@ int main() {
     astl::show(set_1, true);
     astl::show(map_1, true);*/
 
-    // Test adding two containers together
-    /*
-    int aa_size;
-    int *array_added = astl::add(array_1D, 5, array_1D_2, 8, aa_size);
+    // Test operations on two conatiners
+    /*int aa_size;
+    int *array_added = astl::opr(array_1D, 5, op::add, array_1D_2, 8, aa_size);
     astl::show(array_added, aa_size, true);
-    int aa_size1, aa_size2;
-    int **array2D_added = astl::add(array_2D, 3, 2, array_2D_2, 4, 4, aa_size1, aa_size2);
-    astl::show(array2D_added, aa_size1, aa_size2, true);
-    astl::show(astl::add(vec_1, vec_2), true);
-    astl::show(astl::add(list_1, list_2), true);
-    astl::show(astl::add(set_1, set_2), true);
-    astl::show(astl::add(map_1, map_2), true);*/
+    int aa2_size1, aa2_size2;
+    int **array2D_added = astl::opr(array_2D, 3, 2, op::add, array_2D_2, 4, 4, aa2_size1, aa2_size2);
+    astl::show(array2D_added, aa2_size1, aa2_size2, true);
+    astl::show(astl::opr(vec_2, op::add, vec_1), true);
+    astl::show(astl::opr(list_2, op::add, list_1), true);
+    astl::show(astl::opr(set_1, set_op::sum, set_2), true);*/
 
     // Test operation with constants on containers
     /*
     astl::show(astl::opr(array_1D, 5, op::add, 2), 5, true);
-    astl::show(astl::opr(array_1D, 5, op::subtract, 2), 5, true);
-    astl::show(astl::opr(array_1D, 5, op::multiply, 2), 5, true);
-    astl::show(astl::opr(array_1D, 5, op::divide, 2), 5, true);
-    astl::show(astl::opr(array_1D, 5, op::power, 2), 5, true);
-    astl::show(astl::opr(array_1D, 5, op::mod, 2), 5, true);
     astl::show(astl::opr(array_2D, 3, 2, op::add, 2), 3, 2, true);
-    astl::show(astl::opr(array_2D, 3, 2, op::subtract, 2), 3, 2, true);
-    astl::show(astl::opr(array_2D, 3, 2, op::multiply, 2), 3, 2, true);
-    astl::show(astl::opr(array_2D, 3, 2, op::divide, 2), 3, 2, true);
-    astl::show(astl::opr(array_2D, 3, 2, op::power, 2), 3, 2, true);
-    astl::show(astl::opr(array_2D, 3, 2, op::mod, 2), 3, 2, true);
     astl::show(astl::opr(vec_1, op::add, 2), true);
-    astl::show(astl::opr(vec_1, op::subtract, 2), true);
-    astl::show(astl::opr(vec_1, op::multiply, 2), true);
-    astl::show(astl::opr(vec_1, op::divide, 2), true);
-    astl::show(astl::opr(vec_1, op::power, 2), true);
-    astl::show(astl::opr(vec_1, op::mod, 2), true);
     astl::show(astl::opr(list_1, op::add, 2), true);
-    astl::show(astl::opr(list_1, op::subtract, 2), true);
-    astl::show(astl::opr(list_1, op::multiply, 2), true);
-    astl::show(astl::opr(list_1, op::divide, 2), true);
-    astl::show(astl::opr(list_1, op::power, 2), true);
-    astl::show(astl::opr(list_1, op::mod, 2), true);
     astl::show(astl::opr(set_1, op::add, 2), true);
-    astl::show(astl::opr(set_1, op::subtract, 2), true);
-    astl::show(astl::opr(set_1, op::multiply, 2), true);
-    astl::show(astl::opr(set_1, op::divide, 2), true);
-    astl::show(astl::opr(set_1, op::power, 2), true);
-    astl::show(astl::opr(set_1, op::mod, 2), true);
-    astl::show(astl::opr(map_1, op::add, 2), true);
-    astl::show(astl::opr(map_1, op::subtract, 2), true);
-    astl::show(astl::opr(map_1, op::multiply, 2), true);
-    astl::show(astl::opr(map_1, op::divide, 2), true);
-    astl::show(astl::opr(map_1, op::power, 2), true);
-    astl::show(astl::opr(map_1, op::mod, 2), true);*/
+    astl::show(astl::opr(map_1, op::add, 2), true);*/
 
     // Free memory to prevent leaks
     delete[] array_1D;
